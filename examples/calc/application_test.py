90c71ee40cb480dbcda7c76f8a0300c61fc9970e
Tarcisio Fischer <tarcisio.fischer@codethink.co.uk>
2022-10-07 11:02:02 +0100

    Add env vars to application test example setup

--- examples/calc/application_test.py
+++ examples/calc/application_test.py
@@ -6,12 +6,14 @@ import time
 
 @pytest.fixture
 def runapp():
+    assert 'TICK_PATH' in os.environ, 'You must set "TICK_PATH" env var to the libqt_tick dynamic library folder'
+    assert 'APP_PATH' in os.environ, 'You must set "APP_PATH" env var to the example-calculator executable folder'
+
+    tick_path = os.environ.get('TICK_PATH')
+    app_path = os.environ.get('APP_PATH')
     os.environ['QT_TICK_SERVICE_NAME'] = 'AppTesting'
-
-    # TODO: Properly search for binaries (paths missing)
-    os.environ['LD_PRELOAD'] = 'libqt_tick.so'
-    cmd = "example-calculator"
-
+    os.environ['LD_PRELOAD'] = f'{tick_path}/libqt_tick.so'
+    cmd = f'{app_path}/example-calculator'
     app = subprocess.Popen([cmd], stdout=subprocess.PIPE, env=os.environ)
 
     # Wait startup
@@ -23,10 +25,10 @@ def runapp():
 
 def test_application(runapp):
     bus = dbus.SessionBus()
-    proxy = bus.get_object('qt_tick.AppTesting', "/QtTick")
+    proxy = bus.get_object('qt_tick.AppTesting', '/QtTick')
     proxy.invokeQtMethod('btn_4', 'click')
     proxy.invokeQtMethod('btn_0', 'click')
     proxy.invokeQtMethod('btn_+', 'click')
     proxy.invokeQtMethod('btn_2', 'click')
     proxy.invokeQtMethod('btn_=', 'click')
-    assert proxy.invokeTickMethod('output', 'QLineEdit::text') == "42"
+    assert proxy.invokeTickMethod('output', 'QLineEdit::text') == '42'
